ea3a63d6d164b77bd2ee07519e90eaffb43707fb
muhammadsth <68212764+muhammadsth@users.noreply.github.com>
2020-07-21 15:49:29 -0400

    Add files via upload
    Successfully added the add workbook function that outputs the workbook and worksheet along with the specified names.

--- Quizzing_App.py
+++ Quizzing_App.py
@@ -1,29 +1,28 @@
 from openpyxl import Workbook as p
 import openpyxl as P
 from openpyxl.workbook import Workbook
-#from openpyxl.workbook import load_workbook
+# from openpyxl.workbook import load_workbook
 from os import path
 
 wb = Workbook()
-wb.title = "Orgo"
 ws = wb.active
+wb.title = "Orgo"
+
 ws.title = "Chapter 1"
 
 ws1 = wb.create_sheet("Chapter 2")
 ws2 = wb.create_sheet("Chapter 3")
 ws3 = wb.create_sheet("Chapter 4")
 
-
-
-d = ws.cell(4,1,"Ikhla3")
+d = ws.cell(4, 1, "Ikhla3")
 
 for row in ws.values:
     for value in row:
         print(value)
 
-for x in range(1,4):
-    for y in range (1,4):
-        ws.cell(x,y)
+for x in range(1, 4):
+    for y in range(1, 4):
+        ws.cell(x, y)
 
 
 def load_workbook(wb_path):
@@ -38,25 +37,38 @@ wb2 = load_workbook("Microbiology.xlsx")
 for row in ws.iter_rows(min_row=1, max_col=3, max_row=4, values_only=True):
     print(row)
 
-'''
+"""
 for sheet in wb:
     print(sheet.title)
-'''
+"""
+
+
+def new_workbook(namewb: str, namews: str):
+    '''Makes a new workbook for a new subject you would like to be tested on. Allows you to name the workbook and
+    your first sheet.'''
+    wb = Workbook()
+    ws = wb.active
+    ws.title = namews
+    wb.save(namewb + ".xlsx")
+
+
+"""
+new_workbook("Microbiology", "Chapter1")
+"""  # This function has been tested and works because it created mirobiology
+
+
+def add_worksheet(namewb: str, namews: str):
+    """Adds a worksheet to an existing workbook"""
 
 
 
-def new_workbook():
-    '''Makes a new workbook for a new subject you would like to be tested on'''
+
+def new_question(question: str):
     ...
 
-def new_question(question:str):
-    ...
+
 def new_ans():
     ...
 
 
-
-
-
-
-
+wb1 = Workbook(True, True)
